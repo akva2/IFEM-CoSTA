f7e94fe41d2631d8cad9064795f568afbcbd8c0e
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2021-09-24 10:07:35 +0200

    add some simple tests

---
+++ Test/test_AdvectionDiffusion.py
@@ -0,0 +1,21 @@
+import IFEM_CoSTA
+import numpy as np
+
+def testAdvectionDiffusion():
+    ad = IFEM_CoSTA.AdvectionDiffusion('Square-abd1-ad.xinp')
+    assert(ad.ndof == 9)
+
+    mu = {'dt' : 1.0}
+    uprev = [0.0]*ad.ndof
+
+    upred = ad.predict(mu, uprev)
+    np.testing.assert_allclose(upred, [0.0, 0.0, 0.0, 0.0, 0.06749999999999995, 0.0, 0.0, 0.0, 0.0])
+
+    sigma = ad.residual(mu, uprev, upred)
+    np.testing.assert_almost_equal(np.linalg.norm(sigma), 0.0)
+
+    ucorr = ad.correct(mu, uprev, sigma)
+    np.testing.assert_allclose(upred, ucorr)
+
+    dofs = ad.dirichlet_dofs()
+    np.testing.assert_array_equal(dofs, [1,2,3,4,6,7,8,9])
