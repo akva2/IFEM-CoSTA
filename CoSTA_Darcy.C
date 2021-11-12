aa69e6c26a1cb699ab5b7e7cbd47a7407ecaf5d3
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2021-11-12 13:32:30 +0100

    add support for two-field Darcy

--- CoSTA_Darcy.C
+++ CoSTA_Darcy.C
@@ -16,12 +16,66 @@
 
 #include "CoSTAModule.h"
 #include "Darcy.h"
+#include "MixedDarcy.h"
 #include "SIMDarcy.h"
 
 #include "AlgEqSystem.h"
+#include "ASMmxBase.h"
 #include "ElmMats.h"
+#include "ExprFunctions.h"
 #include "SIMconfigure.h"
 #include "SystemMatrix.h"
+#include "TimeIntUtils.h"
+#include "Utilities.h"
+
+#include <tinyxml.h>
+
+
+/*!
+ * \brief Preparsing to check the Darcy formulation to use.
+*/
+
+class DarcyPreParse : public XMLInputBase
+{
+public:
+  bool twofield = false; //!< True to use a two-field formulation
+  int torder = 0; //!< Time integration order
+
+  //! \brief Constructor.
+  DarcyPreParse(const std::string& file)
+  {
+    this->readXML(file.c_str(), false);
+  }
+
+protected:
+  //! \brief Parse an XML element.
+  bool parse(const TiXmlElement* elem)
+  {
+    if (!strcasecmp(elem->Value(),"timestepping")) {
+      std::string type;
+      if (utl::getAttribute(elem,"type",type))
+        torder = TimeIntegration::Order(TimeIntegration::get(type));
+    } else if (!strcasecmp(elem->Value(),"darcy")) {
+      utl::getAttribute(elem,"twofield",twofield);
+      ASMmxBase::Type = ASMmxBase::NONE;
+      const char* formulation = elem->Attribute("formulation");
+      if (formulation) {
+        if (strcasecmp(formulation, "th" ) == 0 ||
+            strcasecmp(formulation, "mixed") == 0) {
+          ASMmxBase::Type = ASMmxBase::REDUCED_CONT_RAISE_BASIS2;
+          twofield = true;
+        } else if (strcasecmp(formulation,"frth") == 0 ||
+                   strcasecmp(formulation,"mixed_full") == 0) {
+          ASMmxBase::Type = ASMmxBase::FULL_CONT_RAISE_BASIS2;
+          twofield = true;
+        }
+    }
+  }
+
+  return true;
+}
+
+};
 
 
 /*!
@@ -33,8 +87,8 @@ class DarcyCoSTA : public Darcy
 public:
   //! \brief The default constructor initializes all pointers to zero.
   //! \param[in] n Number of spatial dimensions
-  explicit DarcyCoSTA(unsigned short int n) :
-    Darcy(n, 1)
+  explicit DarcyCoSTA(unsigned short int n, int torder) :
+    Darcy(n, torder)
   {
   }
 
@@ -53,6 +107,71 @@ public:
 
     return true;
   }
+
+  //! \brief Set a parameter in the source and flux functions.
+  //! \param name Name of parameter
+  //! \param value Value of parameter
+  void setParam(const std::string& name, double value) override
+  {
+    VecFuncExpr* f = dynamic_cast<VecFuncExpr*>(permvalues);
+    if (f)
+      f->setParam(name, value);
+
+    EvalFunction* fs = dynamic_cast<EvalFunction*>(source);
+    if (fs)
+      fs->setParam(name,value);
+    fs = dynamic_cast<EvalFunction*>(porosity);
+    if (fs)
+      fs->setParam(name,value);
+    fs = dynamic_cast<EvalFunction*>(dispersivity);
+    if (fs)
+      fs->setParam(name,value);
+    fs = dynamic_cast<EvalFunction*>(source);
+    if (fs)
+      fs->setParam(name,value);
+  }
+};
+
+
+/*!
+ \brief Integrand for two-field Darcy with CoSTA additions.
+*/
+
+class MixedDarcyCoSTA : public MixedDarcy
+{
+public:
+  //! \brief The default constructor initializes all pointers to zero.
+  //! \param[in] n Number of spatial dimensions
+  explicit MixedDarcyCoSTA(unsigned short int n, int torder) :
+    MixedDarcy(n, torder)
+  {
+  }
+
+  //! \brief Set a parameter in the source and flux functions.
+  //! \param name Name of parameter
+  //! \param value Value of parameter
+  void setParam(const std::string& name, double value) override
+  {
+    VecFuncExpr* f = dynamic_cast<VecFuncExpr*>(permvalues);
+    if (f)
+      f->setParam(name, value);
+
+    EvalFunction* fs = dynamic_cast<EvalFunction*>(source);
+    if (fs)
+      fs->setParam(name,value);
+    fs = dynamic_cast<EvalFunction*>(porosity);
+    if (fs)
+      fs->setParam(name,value);
+    fs = dynamic_cast<EvalFunction*>(dispersivity);
+    if (fs)
+      fs->setParam(name,value);
+    fs = dynamic_cast<EvalFunction*>(source);
+    if (fs)
+      fs->setParam(name,value);
+    fs = dynamic_cast<EvalFunction*>(sourceC);
+    if (fs)
+      fs->setParam(name,value);
+  }
 };
 
 
@@ -67,13 +186,40 @@ class SIMDarcyCoSTA : public SIMDarcy<Dim>,
 public:
   //! \brief Constructor.
   //! \param integrand Reference to integrand to use
-  explicit SIMDarcyCoSTA(DarcyCoSTA& integrand) :
-    SIMDarcy<Dim>(integrand)
+  explicit SIMDarcyCoSTA(Darcy& integrand,
+                         const std::vector<unsigned char>& nf) :
+    SIMDarcy<Dim>(integrand, nf)
   {
   }
 
-  //! \brief Currently unused.
-  void setParam(const std::string&, double) {}
+  void setParam(const std::string& name, double value)
+  {
+    this->drc.setParam(name, value);
+    if (this->mySol) {
+      for (size_t i = 0; i < 2; ++i) {
+        EvalFunction* f = dynamic_cast<EvalFunction*>(this->mySol->getScalarSol(i));
+        if (f)
+          f->setParam(name, value);
+      }
+
+      for (size_t i = 0; i < 2; ++i) {
+        VecFuncExpr* v = dynamic_cast<VecFuncExpr*>(this->mySol->getScalarSecSol(0));
+        if (v)
+          v->setParam(name, value);
+      }
+
+      for (auto& it : this->myScalars) {
+        EvalFunction* f = dynamic_cast<EvalFunction*>(it.second);
+        if (f)
+          f->setParam(name, value);
+      }
+      for (auto& it : this->myScalars) {
+        VecFuncExpr* v = dynamic_cast<VecFuncExpr*>(it.second);
+        if (v)
+          v->setParam(name, value);
+      }
+    }
+  }
 
   //! \brief Returns analytical solutions projected on primary basis.
   //! \param t Time to evaluate at
@@ -106,8 +252,21 @@ struct CoSTASIMAllocator<SIMDarcyCoSTA> {
   void allocate(std::unique_ptr<SIMDarcyCoSTA<Dim>>& newModel, SIMbase*& model,
                 SIMsolution*& solModel, const std::string& infile)
   {
-    integrand = std::make_unique<DarcyCoSTA>(Dim::dimension);
-    newModel = std::make_unique<SIMDarcyCoSTA<Dim>>(*integrand);
+    DarcyPreParse preparse(infile);
+
+    std::vector<unsigned char> nf;
+    if (preparse.twofield) {
+      if (ASMmxBase::Type == ASMmxBase::FULL_CONT_RAISE_BASIS2 ||
+          ASMmxBase::Type == ASMmxBase::REDUCED_CONT_RAISE_BASIS2)
+        nf = {1,1};
+      else
+        nf = {2};
+      integrand = std::make_unique<MixedDarcyCoSTA>(Dim::dimension, preparse.torder);
+    } else {
+      nf = {1};
+      integrand = std::make_unique<DarcyCoSTA>(Dim::dimension, preparse.torder);
+    }
+    newModel = std::make_unique<SIMDarcyCoSTA<Dim>>(*integrand, nf);
     model = newModel.get();
     solModel = newModel.get();
     if (ConfigureSIM(static_cast<SIMDarcy<Dim>&>(*newModel),
@@ -115,7 +274,7 @@ struct CoSTASIMAllocator<SIMDarcyCoSTA> {
       throw std::runtime_error("Error reading input file");
   }
 
-  std::unique_ptr<DarcyCoSTA> integrand; //!< Pointer to integrand instance
+  std::unique_ptr<Darcy> integrand; //!< Pointer to integrand instance
 };
 
 
--- Test/test_Darcy.py
+++ Test/test_Darcy.py
@@ -28,3 +28,27 @@ def testDarcy():
     np.testing.assert_allclose(anasol['secondary_x'], [0.0, 0.0, 0.0, -0.25, 0.0, 0.25, 0.0, -0.0, 0.0])
     assert('secondary_y' in anasol.keys())
     np.testing.assert_allclose(anasol['secondary_y'], [0.0, -0.25, 0.0, 0.0, 0.0, -0.0, 0.0, 0.25, 0.0])
+
+def testDarcyTwoField():
+    darcy = IFEM_CoSTA.Darcy('DarcyTwoFieldSquare.xinp')
+
+    assert(darcy.ndof == 18)
+
+    mu = {'dt' : 1.0,
+          'scale' : 1.0,
+          'phi': 1.0,
+          'D': 1.0}
+    uprev = np.array([0.0]*darcy.ndof)
+
+    upred = darcy.predict(mu, uprev)
+    print(upred)
+    np.testing.assert_allclose(upred, [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.078125,0.07798861480220373,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+
+    sigma = darcy.residual(mu, uprev, upred)
+    np.testing.assert_almost_equal(np.linalg.norm(sigma), 0.0)
+
+    ucorr = darcy.correct(mu, uprev, sigma)
+    np.testing.assert_allclose(upred, ucorr)
+
+    dofs = darcy.dirichlet_dofs()
+    np.testing.assert_array_equal(dofs, [1,2,3,4,5,6,7,8,11,12,13,14,15,16,17,18])
