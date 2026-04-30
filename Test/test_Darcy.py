9daa2df2da72c93e03edb5d4dd3936552d49750a
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2026-04-30 06:51:33 +0200

    Fixed: Only "expression"-type property functions can take parameter names. Update reference values due to missed interpretation of the phi value.

--- Test/test_Darcy.py
+++ Test/test_Darcy.py
@@ -41,8 +41,7 @@ def testDarcyTransport():
     uprev = np.array([0.0]*darcy.ndof)
 
     upred = darcy.predict(mu, uprev)
-    print(upred)
-    np.testing.assert_allclose(upred, [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.078125,0.07798861480220373,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+    np.testing.assert_allclose(upred, [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.078125,0.07495440729587621,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
 
     sigma = darcy.residual(mu, uprev, upred)
     np.testing.assert_almost_equal(np.linalg.norm(sigma), 0.0)
@@ -53,4 +52,4 @@ def testDarcyTransport():
     dofs = darcy.dirichlet_dofs()
     np.testing.assert_array_equal(dofs, [1,2,3,4,5,6,7,8,11,12,13,14,15,16,17,18])
     qi = darcy.qi(mu, ucorr, 'no_concentration_zone')
-    np.testing.assert_allclose(qi, [0.01949715369980741])
+    np.testing.assert_allclose(qi, [0.01873860182343269])
